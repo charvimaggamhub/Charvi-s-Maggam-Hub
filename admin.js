ddb2dd3e905445ab105ab764cb7743d3bb1ce454
Rajana <rajananikhil9@gmail.com>
2026-03-04 16:55:38 +0530

    Full updated working version

--- admin.js
+++ admin.js
@@ -10,15 +10,6 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
 
-// ================= SUPABASE CONFIG =================
-const SUPABASE_URL = "https://syciwtjfsvgggljorxbm.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5Y2l3dGpmc3ZnZ2dsam9yeGJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2MDA3NzQsImV4cCI6MjA4ODE3Njc3NH0.QMx67QYQ-PDh3ZciJnu0PTCkUMhBMV2q57AGF5t2E3Q";
-
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
-
 
 // 🔐 Only this email can access admin
 const ADMIN_EMAIL = "admin@charvi.com";
