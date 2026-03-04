1b7ceaa72e7726c8f8c58cf36a17234127f0d66b
Rajana <rajananikhil9@gmail.com>
2026-03-04 17:54:22 +0530

    Full updated working version

--- booking.js
+++ booking.js
@@ -1,14 +1,3 @@
-// ================= SUPABASE CONFIG =================
-
-// Replace with your Supabase details
-const SUPABASE_URL = "https://syciwtjfsvgggljorxbm.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5Y2l3dGpmc3ZnZ2dsam9yeGJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2MDA3NzQsImV4cCI6MjA4ODE3Njc3NH0.QMx67QYQ-PDh3ZciJnu0PTCkUMhBMV2q57AGF5t2E3Q";
-
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
-
 
 // ================= BOOKING FORM SUBMIT =================
 
